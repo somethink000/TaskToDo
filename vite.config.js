e61ec2fb24a34e8ec5c213f43c83b0bf7af84dc4
some <zubkoffalex4nder@yandex.ru>
2024-09-05 20:29:22 +0300

    ok

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,14 @@ export default defineConfig({
       '@': '/resources/js',
     },
   },
+  server: {
+    hmr: {
+        host: "0.0.0.0",
+    },
+    port: 3000,
+    host: true,
+},
+
   build: {
     sourcemap: true,
   },
