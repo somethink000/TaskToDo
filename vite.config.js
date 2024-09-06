4795f4105ed18cafdaf0813df642606a0319349c
some <zubkoffalex4nder@yandex.ru>
2024-09-06 14:32:45 +0300

    works fine

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,22 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-    server: {
-        hmr: {
-            host: "0.0.0.0",
+        server: {
+            host: '0.0.0.0',
+            port: 5173,
+            hmr: {
+                host: 'localhost',
+            },
+            watch: {
+                usePolling: true,
+            },
         },
-        port: 3000,
-        host: true,
-    },
+    
     plugins: [
-        
         laravel({
-            input: 'resources/js/app.js',
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
         vue({
@@ -25,4 +28,9 @@ export default defineConfig({
             },
         }),
     ],
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+        },
+    },
 });
