c65bef8091255bed66399bb6b5949b2de5d46a77
some <zubkoffalex4nder@yandex.ru>
2024-09-06 11:26:35 +0300

    new setup

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', fn () => view('app'))->name('app');
+Route::get('/', fn () => view('app'))->name('app');
 
 Route::get('/reset-password/{token}', fn () => view('app'))
     ->middleware(['guest:'.config('fortify.guard')])
     ->name('password.reset');
 
-// Route::get('{any}', fn () => view('app'))->where('any', '^((?!api).)*');
+Route::get('{any}', fn () => view('app'))->where('any', '^((?!api).)*');
