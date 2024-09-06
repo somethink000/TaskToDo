36787135ecfd21e0b3980072b7acc86261b117b7
some <zubkoffalex4nder@yandex.ru>
2024-09-06 17:03:04 +0300

    e

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
         server: {
-            host: '0.0.0.0',
+            host: true,
             port: 5173,
             hmr: {
                 host: 'kep.pw',
