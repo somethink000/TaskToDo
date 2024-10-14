4e9d9cdad044ee83635b5b3a28ce0ba52a7d6116
some <zubkoffalex4nder@yandex.ru>
2024-10-14 18:20:47 +0300

    nodes done

--- resources/js/components/nodes/TaskNode.vue
+++ resources/js/components/nodes/TaskNode.vue
@@ -55,6 +55,23 @@ function Remove() {
     
 }
 
+function Done() {
+    //console.log(node.done);
+    node.done = !node.done
+
+    return axios.patch('/api/nodes/' + node.id, node, {
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+    .then(res => {
+        
+        //console.log(res.data);
+        // toggleEdit()
+    })
+    
+}
+
 
 </script>
 
@@ -67,40 +84,46 @@ function Remove() {
 
         <ImageButton @click="toggleEdit()" image="/images/edit.png" size="12" />
         <ImageButton @click="Remove()" image="/images/trash.png" size="12" />
-        <ImageButton image="/images/check.png" size="12" />
+        <ImageButton @click="Done()" image="/images/check.png" size="12" />
 
     </taskActions>
 
 
-    <content>
+    <content v-bind:class="{ 'done' : node.done == true }">
 
-        <taskTitle> 
+        <taskTitle class="boxed"> 
 
             <input v-if="inEdit == true" v-on:keyup.enter="onDataTyped" ref="inp" v-model="data.label"  type="text" placeholder="Text" >
            
             <txt v-else >{{ data.label }}</txt>
 
+           
+            <BaseLine />
+        
+
        </taskTitle>
        
-        
-        <description>
-            
-            <BaseLine />
+         
+       
+    
+    
+        <description class="boxed">
 
             <input v-if="inEdit == true" v-on:keyup.enter="onDataTyped" ref="desc" v-model="data.description"  type="desc" placeholder="Description" >
-           
+            
             <txt v-else >{{ data.description }}</txt>
 
         </description>
-         
-       
+
     </content>
     
-  
     
+    <Handle :position="Position.Top" />
+    <!-- <Handle :position="Position.Bottom" />
+    <Handle :position="Position.Right" />
+    <Handle :position="Position.Left" /> -->
     
-    <Handle type="target" :position="Position.Left" />
-    <Handle type="source" :position="Position.Right" />
+    <!-- <Handle type="source" :position="Position.Right" /> -->
 </template>
 
 <style>
@@ -111,6 +134,9 @@ function Remove() {
         flex-direction: row;
         justify-content: end;
 
+        button {
+            background-color: rgba(34, 34, 34, 0.5);
+        }
         /* position: absolute;
         right: 0;
         padding-bottom: 100px;
@@ -119,16 +145,39 @@ function Remove() {
         z-index: 2; */
     }
 
-    .vue-flow__node {
+
+    .boxed{
         background-color: rgba(34, 34, 34, 0.5);
-        padding: 10px;
         border-radius: 8px;
         box-shadow: 0px 1px 8px 1px rgba(255, 255, 255, 0.2);
+
+    }
+
+
+    .vue-flow__node {
+        
+        padding: 10px;
+       
     }
     .vue-flow__node.selected {
-        box-shadow: 0px 1px 8px 1px rgba(255, 255, 255, 0.8);
+
+        .boxed {
+            box-shadow: 0px 1px 8px 1px rgba(255, 255, 255, 0.8);
+
+        }
+
+        
     }
     
+
+    .done {
+        .boxed {
+            background-color: rgba(34, 34, 34, 0.8);
+            color: rgb(105, 105, 105);
+        }
+        
+    }
+
     content{
         display: flex;
         flex-direction: column;
@@ -141,13 +190,21 @@ function Remove() {
         taskTitle {
             width: 100%;
             display: flex;
-            flex-direction: row;
+            flex-direction: column;
             align-items: center;
             justify-content: space-between;
+            
+            baseBox {
+                padding-top: 4px;
+                padding-bottom: 6px;
+                width: 10%;
+                
+            }
         }
 
         
         description { 
+            margin-top: 12px;
             width: 100%;
         }
         
